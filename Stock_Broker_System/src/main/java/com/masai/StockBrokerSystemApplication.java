f0f149948db1088db4feea88c94a4b8f8896c93e
manoj <manojsahoo890880@gmail.com>
2023-05-03 13:21:06 +0530

    test

---
+++ Stock_Broker_System/src/main/java/com/masai/StockBrokerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBrokerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBrokerSystemApplication.class, args);
+	}
+
+}
